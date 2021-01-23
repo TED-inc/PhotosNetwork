5aa61a15f83c579f955c69c9cb19a777b36e1abb
TEDinc <mihail.bobko@gmail.com>
2021-01-23 15:30:27 +0200

    [+] add test of cashing

---
+++ Assets/Scripts/CashedImageTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace TEDinc.PhotosNetwork
+{
+    public class CashedImageTest : MonoBehaviour
+    {
+        [SerializeField]
+        private LocalDBTest localDB;
+        [SerializeField]
+        private RectTransform imagesParent;
+        [SerializeField]
+        private TMP_Text label;
+
+        private Dictionary<int, RawImage> images = new Dictionary<int, RawImage>();
+
+        private string CashePath => $"{Application.temporaryCachePath}/Images/";
+
+        private string FileInCashePath(string fileName) => 
+            $"{CashePath}{fileName}.jpg";
+
+        public void SaveToCasheFromLocalDB()
+        {
+            if (!Directory.Exists(CashePath))
+                Directory.CreateDirectory(CashePath);
+            foreach (LocalDBTest.Publication publication in localDB.connection.Table<LocalDBTest.Publication>())
+            {
+                label.text += $"publication.Id {publication.Id}\n";
+                File.WriteAllBytes(FileInCashePath(publication.Id.ToString()), publication.PhotoData);
+            }
+            label.text += "!publ!\n";
+        }
+
+        public void LoadFromCasheAndDisplay()
+        {
+            if (!Directory.Exists(CashePath))
+                Directory.CreateDirectory(CashePath);
+            foreach (string cashedImagePath in Directory.GetFiles(CashePath))
+            {
+                int id = Convert.ToInt32(Path.GetFileNameWithoutExtension(cashedImagePath));
+                label.text += $"cashed.Id {id}\n";
+                RawImage rawImage;
+                Texture2D texture = new Texture2D(0, 0);
+
+                if (images.ContainsKey(id))
+                    rawImage = images[id];
+                else
+                {
+                    GameObject imageParent = new GameObject($"img{id}");
+                    imageParent.transform.SetParent(imagesParent);
+                    imageParent.transform.localScale = Vector3.one;
+                    rawImage = imageParent.AddComponent<RawImage>();
+                    images.Add(id, rawImage);
+                }
+                
+                texture.LoadImage(File.ReadAllBytes(cashedImagePath));
+                rawImage.texture = texture;
+                rawImage.rectTransform.sizeDelta = new Vector2(
+                    imagesParent.rect.width, 
+                    (float)texture.height / texture.width * imagesParent.rect.width);
+            }
+            label.text += "!cash!\n";
+        }
+    }
+}
--- Assets/Scripts/LoadImageTest.cs
+++ Assets/Scripts/LoadImageTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace TEDinc.PhotosNetwork
 {
@@ -10,12 +11,14 @@ namespace TEDinc.PhotosNetwork
         private LocalDBTest localDBTest;
         [SerializeField]
         private RawImage image;
+        [SerializeField]
+        private TMP_Text label;
 
         private string CashePath => Application.temporaryCachePath + "/f.jpg";
 
         private void Start()
         {
-            LoadImageFromCashe();
+            //LoadImageFromCashe();
         }
 
         private void LoadImageFromCashe()
@@ -25,7 +28,7 @@ namespace TEDinc.PhotosNetwork
             LocalDBTest.Publication publication = localDBTest.connection.Table<LocalDBTest.Publication>().FirstOrDefault();
             if (publication == null || publication.PhotoData == null)
             {
-                Debug.LogError("!");
+                Debug.LogError("! p.null:" + (publication == null));
                 return;
             }
             byte[] data = publication.PhotoData;
@@ -37,14 +40,20 @@ namespace TEDinc.PhotosNetwork
 
         public void LoadImageFromGalery()
         {
-            NativeGallery.GetImageFromGallery(Callback, "Test loading");
+            Debug.Log("0" + NativeGallery.IsMediaPickerBusy());
+            NativeGallery.Permission permission = NativeGallery.GetImageFromGallery(Callback, "Test loading");
+            Debug.Log("1" + NativeGallery.IsMediaPickerBusy() + permission);
 
             void Callback(string path)
             {
+                Debug.Log("2");
                 Texture2D textureFrom = NativeGallery.LoadImageAtPath(path, maxSize: 2048, generateMipmaps: false, markTextureNonReadable: false);
                 byte[] data = textureFrom.EncodeToJPG();
+                label.text += $"path {path}\n";
                 localDBTest.connection.Insert(new LocalDBTest.Publication(1, "TestMessage", data));
+                label.text += $"count {localDBTest.connection.Table<LocalDBTest.Publication>().Count()}\n";
                 LoadImageFromCashe();
+                Debug.Log("3");
             }
         }
     }
